065b9334f4581efecaeef7d279a2e1b2680087bc
Lloyd Pick <lloydpick@gmail.com>
2016-01-19 18:19:47 +0000

    runner start

---
+++ lib/runner.rb
@@ -0,0 +1,28 @@
+module Uphold
+  class Runner
+    include Logging
+
+    def initialize(config:)
+      @name = config[:name]
+      @engine = config[:engine][:klass]
+      @transport = config[:transport][:klass]
+      @config = config
+    end
+
+    def start
+      transport_params = @config[:transport][:settings]
+      transport_params.merge!(dir: Dir.mktmpdir)
+
+      transport = @transport.new(transport_params)
+
+      engine_params = @config[:engine]
+      engine_params.delete(:type)
+      engine_params.delete(:klass)
+      engine_params.merge!(path: transport.fetch)
+
+      engine = @engine.new(engine_params)
+      engine.recover
+    end
+
+  end
+end
