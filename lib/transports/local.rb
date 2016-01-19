bd3edd4fc5f3d3aaec49daf3106d9198304248cd
Lloyd Pick <lloydpick@gmail.com>
2016-01-19 18:19:52 +0000

    flesh out local file

--- lib/transports/local.rb
+++ lib/transports/local.rb
@@ -1,13 +1,30 @@
 module Uphold
   module Transports
     class Local < Transport
-      def initialize(path:)
-        @path = path
+      def initialize(params)
+        @dir = params[:dir]
+        @path = params[:path]
+        @folder = params[:folder]
       end
 
       def fetch
-        @path
+        if File.file?(@path)
+          tmp_path = File.join(@dir, File.basename(@path))
+          FileUtils.cp(@path, tmp_path)
+          @path = tmp_path
+          logger.error @path
+
+          decompress(@path) do |decompressed_file|
+            logger.info decompressed_file
+          end
+
+          logger.info 'done decompress'
+          File.join(@dir, @folder)
+        else
+          logger.error "No file exists at '#{@path}'"
+        end
       end
+
     end
   end
 end
