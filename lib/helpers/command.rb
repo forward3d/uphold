cccc4d9c13d4241a4db69d65253dd5f1bbabee2e
Lloyd Pick <lloydpick@gmail.com>
2016-01-20 11:51:37 +0000

    command runner

---
+++ lib/helpers/command.rb
@@ -0,0 +1,29 @@
+module Uphold
+  module Command
+    module_function
+
+    def run(cmd)
+      logger.debug "Running command '#{cmd}'"
+      log_command = "#{cmd.split(' ')[0]}"
+      Open3.popen3(cmd) do |_stdin, stdout, stderr, thread|
+        # read each stream from a new thread
+        { out: stdout, err: stderr }.each do |key, stream|
+          Thread.new do
+            until (line = stream.gets).nil? do
+              # yield the block depending on the stream
+              if key == :out
+                logger.debug "[#{log_command}] [stdout] #{line.chomp}" unless line.nil?
+              else
+                logger.error "[#{log_command}] [stderr] #{line.chomp}" unless line.nil?
+              end
+            end
+          end
+        end
+
+        thread.join # don't exit until the external process is done
+        return thread.value
+      end
+    end
+
+  end
+end
