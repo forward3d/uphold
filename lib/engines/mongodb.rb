48a8e84553f358a0a868d82f769aa24e49db52ab
Lloyd Pick <lloydpick@gmail.com>
2016-01-21 14:14:42 +0000

    set a default container for mongo

--- lib/engines/mongodb.rb
+++ lib/engines/mongodb.rb
@@ -1,9 +1,17 @@
 module Uphold
   module Engines
     class Mongodb < Engine
+
+      def initialize(params)
+        super(params)
+        @docker_image ||= 'mongo'
+        @docker_tag ||= '3.2.1'
+        @port ||= 27_017
+      end
+
       def load_backup(path)
         Dir.chdir(path) do
-          run_command("mongorestore --verbose --drop --db uphold #{@database}")
+          run_command("mongorestore --verbose --host #{container_ip_address} --port #{@port} --drop --db uphold #{@database}")
         end
       end
     end
