19541eae7194aaa28d6017eac233c9ccecbd3653
Lloyd Pick <lloydpick@gmail.com>
2016-01-26 19:24:07 +0000

    have the ui actually show something

--- ui.rb
+++ ui.rb
@@ -8,10 +8,15 @@ module Uphold
   class Ui < ::Sinatra::Base
     include Logging
     set :views, settings.root + '/views'
+    set :public_folder, settings.root + '/public'
 
     helpers do
       def h(text)
         Rack::Utils.escape_html(text)
+      end
+
+      def epoch_to_datetime(epoch)
+        Time.at(epoch).utc.to_datetime
       end
     end
 
@@ -22,17 +27,30 @@ module Uphold
     end
 
     get '/' do
+      @logs = logs
       erb :index
     end
 
     get '/run/:slug' do
+      start_docker_container(params[:slug])
+      redirect '/'
+    end
+
+    get '/logs/:filename' do
+      # refactor, dangerous
+      File.open(File.join('/var/log/uphold', params[:filename]))
+    end
+
+    private
+
+    def start_docker_container(slug)
       if Docker::Image.exist?("#{UPHOLD[:docker_container]}:#{UPHOLD[:docker_tag]}")
         Docker::Image.get("#{UPHOLD[:docker_container]}:#{UPHOLD[:docker_tag]}")
       else
         Docker::Image.create('fromImage' => UPHOLD[:docker_container], 'tag' => UPHOLD[:docker_tag])
       end
 
-      volumes = { }
+      volumes = {}
       UPHOLD[:docker_mounts].flatten.each { |m| volumes[m] = { "#{m}" => 'ro' } }
 
       # this is a hack for when you're working in development on osx
@@ -47,13 +65,42 @@ module Uphold
 
       @container = Docker::Container.create(
         'Image' => "#{UPHOLD[:docker_container]}:#{UPHOLD[:docker_tag]}",
-        'Cmd' => [params[:slug] + '.yml'],
+        'Cmd' => [slug + '.yml'],
         'Volumes' => volumes,
-        'Env' => ["UPHOLD_LOG_FILENAME=#{Time.now.to_i}_#{params[:slug]}"]
+        'Env' => ["UPHOLD_LOG_FILENAME=#{Time.now.to_i}_#{slug}"]
       )
 
       @container.start('Binds' => volumes.map { |v, h| "#{v}:#{h.keys.first}" })
-      redirect '/'
+    end
+
+    def logs
+      logs = {}
+      raw_test_logs.each do |log|
+        epoch = log.split('_')[0]
+        config = log.split('_')[1].gsub!('.log', '')
+        state = raw_state_files.find { |s| s.include?("#{epoch}_#{config}") }
+        if state
+          state = state.gsub("#{epoch}_#{config}", '')[1..-1]
+        else
+          state = 'running'
+        end
+        logs[config] ||= []
+        logs[config] << { epoch: epoch.to_i, state: state, filename: log }
+        logs[config].sort_by! { |h| h[:epoch].to_i }.reverse!
+      end
+      logs
+    end
+
+    def raw_test_logs
+      raw_files.select { |file| File.extname(file) == '.log' }
+    end
+
+    def raw_state_files
+      raw_files.select { |file| File.extname(file) == '' }
+    end
+
+    def raw_files
+      Dir[File.join('/var/log/uphold', '*')].select { |log| File.basename(log) =~ /^[0-9]{10}/ }.map { |file| File.basename(file) }
     end
   end
 end
