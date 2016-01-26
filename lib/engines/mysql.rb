c45591d96b9652e2669a11235cf0990718bbc65f
Lloyd Pick <lloydpick@gmail.com>
2016-01-26 16:05:56 +0000

    mysql engine

--- lib/engines/mysql.rb
+++ lib/engines/mysql.rb
@@ -1,8 +1,20 @@
 module Uphold
   module Engines
     class Mysql < Engine
-      def load_backup(path:)
-        # do stuff
+
+      def initialize(params)
+        super(params)
+        @docker_image ||= 'mysql'
+        @docker_tag ||= '5.7.10'
+        @docker_env ||= ['MYSQL_ALLOW_EMPTY_PASSWORD=yes', "MYSQL_DATABASE=#{@database}"]
+        @port ||= 3306
+        @sql_file ||= 'MySQL.sql'
+      end
+
+      def load_backup(path)
+        Dir.chdir(path) do
+          run_command("mysql -u root --host=#{container_ip_address} #{@database} < #{@sql_file}")
+        end
       end
     end
   end
