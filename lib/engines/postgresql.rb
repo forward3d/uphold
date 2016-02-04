76eb5519299a43a4f200f970996e60b9788bd81e
Lloyd Pick <lloydpick@gmail.com>
2016-02-04 15:16:50 +0000

    postgres engine support

---
+++ lib/engines/postgresql.rb
@@ -0,0 +1,21 @@
+module Uphold
+  module Engines
+    class Postgresql < Engine
+
+      def initialize(params)
+        super(params)
+        @docker_image ||= 'postgres'
+        @docker_tag ||= '9.5.0'
+        @docker_env ||= ["POSTGRES_USER=#{@database}", "POSTGRES_DB=#{@database}"]
+        @port ||= 5432
+        @sql_file ||= 'PostgreSQL.sql'
+      end
+
+      def load_backup(path)
+        Dir.chdir(path) do
+          run_command("psql --no-password --set ON_ERROR_STOP=on --username=#{@database} --host=#{container_ip_address} --port=#{@port} --dbname=#{@database} < #{@sql_file}")
+        end
+      end
+    end
+  end
+end
